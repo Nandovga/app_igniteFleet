6bde4db1b5438b54d56fd49825a54c555d373b54
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2023-11-04 16:13:32 -0300

    Geolocalização

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,16 +1,26 @@
-import {useRef, useState} from "react";
+import {Car} from "phosphor-react-native";
+import {useEffect, useRef, useState} from "react";
 import {useNavigation} from "@react-navigation/native";
 import {Alert, ScrollView, TextInput} from "react-native";
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
+import {
+    LocationAccuracy,
+    LocationSubscription,
+    useForegroundPermissions,
+    watchPositionAsync
+} from "expo-location";
 
 import {useUser} from "@realm/react";
 import {useRealm} from "../../libs/realm/index";
 import {Historic} from "../../libs/realm/schemas/Historic";
+import {Container, Content, Message} from './styles'
 
-import {Container, Content} from './styles'
 import {Header} from "../../components/Header";
 import {Button} from "../../components/Button";
+import {Loading} from "../../components/Loading";
+import {LocationInfo} from "../../components/LocationInfo";
 import {TextAreaInput} from "../../components/TextAreaInput";
+import {getAddressLocation} from "../../utils/getAddressLocation";
 import {LicensePlateInput} from "../../components/LicensePlateInput";
 import {licensePlateValidate} from "../../utils/licensePlateValidate";
 
@@ -22,6 +32,10 @@ export function Departure() {
     const [description, setDescription] = useState('');
     const [licensePlate, setLicensePlate] = useState('')
     const [isRegistering, setIsRegistering] = useState(false)
+    const [isLoadingLocation, setIsLoadingLocation] = useState(true)
+    const [currentAddress, setCurrentAddress] = useState<string | null>(null)
+
+    const [locationForegroundPermission, requestLocationForegroundPermission] = useForegroundPermissions()
 
     const {goBack} = useNavigation()
     const realm = useRealm();
@@ -31,7 +45,7 @@ export function Departure() {
     const descriptionRef = useRef<TextInput>(null)
 
     function handleDepartureRegister() {
-        try{
+        try {
             if (!licensePlateValidate(licensePlate)) {
                 licensePlateRef.current?.focus()
                 return Alert.alert('Placa inválida', 'A placa é inválida. Por favor, informe a placa correta do veículo.')
@@ -58,12 +72,58 @@ export function Departure() {
         }
     }
 
+    useEffect(() => {
+        requestLocationForegroundPermission();
+    }, []);
+
+    useEffect(() => {
+        if (!locationForegroundPermission?.granted)
+            return;
+
+        let subscriptions: LocationSubscription;
+        watchPositionAsync({
+            accuracy: LocationAccuracy.High,
+            timeInterval: 1000
+        }, (location) => {
+            getAddressLocation(location.coords)
+                .then((address) => {
+                    if (address)
+                        setCurrentAddress(address)
+                })
+                .finally(() => setIsLoadingLocation(false))
+        }).then(response => subscriptions = response)
+
+        return () => {
+            if (subscriptions)
+                subscriptions.remove()
+        };
+    }, [locationForegroundPermission]);
+
+    //Verificando a permissão de localização
+    if (!locationForegroundPermission?.granted)
+        return (
+            <Container>
+                <Header title="Saída"/>
+                <Message>
+                    Você precisa permitir que o aplicativo tenha acesso a localização para utilizar essa funcionalidade.
+                    Por favor, acesse as configurações do seu dispositivo para conceder essa permissão ao aplicativo.
+                </Message>
+            </Container>
+        )
+
+    if (isLoadingLocation)
+        return <Loading/>
+
     return (
         <Container>
             <Header title="Saída"/>
             <KeyboardAwareScrollView extraHeight={100}>
                 <ScrollView>
                     <Content>
+                        {currentAddress &&
+                            <LocationInfo label="Localização Atual"
+                                          description={currentAddress}
+                                          icon={Car}/>}
                         <LicensePlateInput
                             ref={licensePlateRef}
                             label="Placa do veículo"
